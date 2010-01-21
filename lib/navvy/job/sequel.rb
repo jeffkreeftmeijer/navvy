7f58e518ca0a94d06886c6d51c5b83c8a0059b37
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-01-21 18:53:33 +0100

    added sqlite3-ruby dependency, put `require "yaml"` back in the sequel setup and fixed it for 1.9

--- lib/navvy/job/sequel.rb
+++ lib/navvy/job/sequel.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sequel'
+require 'yaml'
 
 module Navvy
   class Job < Sequel::Model
@@ -32,7 +33,7 @@ module Navvy
     # Add a job to the job queue.
     #
     # @param [Object] object the object you want to run a method from
-    # @param [Symbol, String] method_name the name of the method you want to 
+    # @param [Symbol, String] method_name the name of the method you want to
     # run
     # @param [*] arguments optional arguments you want to pass to the method
     #
@@ -80,7 +81,7 @@ module Navvy
         filter('`completed_at` IS NOT NULL').delete unless keep?
       end
     end
-    
+
     ##
     # Run the job. Will delete the Navvy::Job record and return its return
     # value if it runs successfully unless Navvy::Job.keep is set. If a job
@@ -104,62 +105,62 @@ module Navvy
         failed(exception.message)
       end
     end
-    
+
     ##
-    # Mark the job as completed. Will set completed_at to the current time and 
+    # Mark the job as completed. Will set completed_at to the current time and
     # optionally add the return value if provided.
     #
     # @param [String] return_value the return value you want to store.
     #
     # @return [true, false] update_attributes the result of the
     # update_attributes call
-    
+
     def completed(return_value = nil)
       update({
         :completed_at =>  Time.now,
         :return =>        return_value
       })
     end
-    
+
     ##
-    # Mark the job as failed. Will set failed_at to the current time and 
+    # Mark the job as failed. Will set failed_at to the current time and
     # optionally add the exception message if provided.
     #
     # @param [String] exception the exception message you want to store.
     #
     # @return [true, false] update_attributes the result of the
     # update_attributes call
-    
+
     def failed(message = nil)
       update({
         :failed_at => Time.now,
         :exception => message
       })
     end
-    
+
     ##
-    # Check if the job has been run. 
+    # Check if the job has been run.
     #
     # @return [true, false] ran
-    
+
     def ran?
       completed? || failed?
     end
-    
+
     ##
     # Check how long it took for a job to complete or fail
     #
     # @return [Time, Integer] time the time it took
-    
+
     def duration
       ran? ? (completed_at || failed_at) - started_at : 0
     end
-    
+
     ##
     # Check if completed_at is set
     #
     # @return [true, false] set?
-    
+
     def completed_at?
       !completed_at.nil?
     end
@@ -168,20 +169,20 @@ module Navvy
     # Check if failed_at is set
     #
     # @return [true, false] set?
-    
+
     def failed_at?
       !failed_at.nil?
     end
-    
+
     ##
     # Get the job arguments as an array
     #
     # @return [array] arguments
-    
+
     def args
-      arguments.first.is_a?(Array) ? arguments : YAML.load(arguments)
+      arguments.is_a?(Array) ? arguments : YAML.load(arguments)
     end
-    
+
     alias_method :completed?, :completed_at?
     alias_method :failed?,    :failed_at?
   end
