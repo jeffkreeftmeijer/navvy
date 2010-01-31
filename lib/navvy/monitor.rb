7ddb93f9fa4333332aa6eb6411ca50557ea48a06
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-01-31 14:53:55 +0100

    added the Navvy::Monitor /:id, /pending, /failed and /completed pages

--- lib/navvy/monitor.rb
+++ lib/navvy/monitor.rb
@@ -7,13 +7,53 @@ module Navvy
     set :views,   File.expand_path(File.dirname(__FILE__) + '/monitor/views')
     set :public,  File.expand_path(File.dirname(__FILE__) + '/monitor/public')
     set :static,  true
-    
-    get '/' do
+
+    before do
       @pending_count =    Job.count(:failed_at => nil, :completed_at => nil)
       @completed_count =  Job.count(:completed_at => {'$ne' => nil})
       @failed_count =     Job.count(:failed_at => {'$ne' => nil})
-      @jobs =             Job.all(:order => 'priority desc, created_at asc', :parent_id => nil, :limit => 100)
+    end
+
+    get '/' do
+      @jobs = Job.all(:order => 'run_at desc, priority desc', :limit => 250)
       haml :index, :layout => true
+    end
+
+    get '/pending' do
+      @jobs = Job.all(
+        :failed_at => nil,
+        :completed_at => nil,
+        :order => 'priority desc, run_at desc',
+        :limit => 250
+      )
+      haml :index, :layout => true
+    end
+
+    get '/completed' do
+      @jobs = Job.all(
+        :completed_at => {'$ne' => nil},
+        :order => 'priority desc, run_at desc',
+        :limit => 250
+      )
+      haml :index, :layout => true
+    end
+
+    get '/failed' do
+      @jobs = Job.all(
+        :failed_at => {'$ne' => nil},
+        :order => 'priority desc, run_at desc',
+        :limit => 250
+      )
+      haml :index, :layout => true
+    end
+
+    get '/:id' do
+      @jobs = Job.all(
+        '$where' => "this._id == '#{params[:id]}' || this.parent_id == '#{params[:id]}'",
+        :order => 'priority desc, run_at desc',
+        :limit => 100
+      )
+      haml :show
     end
   end
 end
