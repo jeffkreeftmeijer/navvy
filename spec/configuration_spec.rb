a892c82f1f967f3de51e4be8b0c330c50a0a5037
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-05-23 19:04:34 +0200

    minor cleanups and added /test.log to .gitignore

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -9,88 +9,64 @@ describe Navvy::Configuration do
       config.sleep_time = 5
     end
   end
-  
+
   it 'should have a job limit of 100 by default' do
     Navvy::Job.limit.should == 100
   end
-  
+
   it 'should set the job limit' do
     Navvy.configure do |config|
       config.job_limit = 10
     end
-    
+
     Navvy::Job.limit.should == 10
   end
-  
+
   it 'should have keep_jobs off by default' do
     Navvy::Job.keep.should == false
   end
-  
+
   it 'should set keep_jobs' do
     Navvy.configure do |config|
       config.keep_jobs = 10
     end
-    
+
     Navvy::Job.keep.should == 10
   end
 
   it 'should log to STDOUT by default' do
     Navvy.logger.instance_variable_get(:@logdev).filename.should == nil
   end
-  
+
   it 'should set the logger' do
     Navvy.configure do |config|
       config.logger = Logger.new('test.log')
     end
-    
+
     Navvy.logger.instance_variable_get(:@logdev).filename.should == 'test.log'
   end
-  
-  #it 'should not have a logger by default' do
-  #  Navvy::Log.logger.should == nil
-  #end
-  #
-  #it 'should set the keep_jobs' do
-  #  Navvy.configure do |config|
-  #    config.logger = :rails
-  #  end
-  #  
-  #  Navvy::Log.logger.should == :rails
-  #end
-  
-  #it 'should be quiet in the specs' do
-  #  Navvy::Log.quiet.should == true
-  #end
-  #
-  #it 'should turn quiet off' do
-  #  Navvy.configure do |config|
-  #    config.quiet = false
-  #  end
-  #  
-  #  Navvy::Log.quiet.should == false
-  #end
-  
+
   it 'should have a default sleep time of 5' do
     Navvy::Worker.sleep_time.should == 5
   end
-  
+
   it 'should turn quiet off' do
     Navvy.configure do |config|
       config.sleep_time = 10
     end
-    
+
     Navvy::Worker.sleep_time.should == 10
   end
-  
+
   it 'should have a default max_attempts of 25' do
     Navvy::Job.max_attempts.should == 25
   end
-  
+
   it 'should set max_attempts to 15' do
     Navvy.configure do |config|
       config.max_attempts = 15
-    end                       
-    
+    end
+
     Navvy::Job.max_attempts.should == 15
   end
 end
