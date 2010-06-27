aa8ea4ab2ce253d26a81e0f92c0d03adbfaafcea
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-06-27 14:23:06 +0200

    added the --sequel and --active_record options to the rails 2 generator

--- generators/navvy/navvy_generator.rb
+++ generators/navvy/navvy_generator.rb
@@ -1,11 +1,17 @@
 class NavvyGenerator < Rails::Generator::Base
+  default_options :orm => 'active_record'
+
   def manifest
     record do |m|
-      options = {
-        :migration_file_name => 'create_jobs'
-      }
-      m.migration_template 'active_record_migration.rb', 'db/migrate', options
+      m.migration_template "#{options[:orm]}_migration.rb", 'db/migrate', {:migration_file_name => 'create_jobs'}
     end
+  end
+
+  def add_options!(opt)
+    opt.separator ''
+    opt.separator 'Options:'
+    opt.on('--active_record', 'Generate a migration file for ActiveRecord. (default)') { options[:orm] = 'active_record' }
+    opt.on('--sequel', 'Generate a migration file for Sequel.') { options[:orm] = 'sequel' }
   end
 
   def banner
